3fd84b8417a579b116e69596678082176ca87365
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-27 00:26:41 +0200

    create base imports and root instance

---
+++ converter.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+import urllib.request
+import json
+from tkinter import messagebox
+
+root = Tk()
+root.title('Конвертер валют')
+root.geometry('300x250')
+root.resizable(False, False)
+
+root.mainloop()
