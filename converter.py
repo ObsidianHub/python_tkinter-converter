cd492ffd45d8e8a22e6f7e862b5518ff27ffb567
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-28 23:14:22 +0200

    create header Frame

--- converter.py
+++ converter.py
@@ -6,7 +6,9 @@ from tkinter import messagebox
 
 root = Tk()
 root.title('Конвертер валют')
-root.geometry('300x250')
+root.geometry('300x250+1000+300')
 root.resizable(False, False)
+
+START_AMOUNT = 1000
 
 root.mainloop()
